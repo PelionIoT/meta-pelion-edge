f0927e19e09e76f20e44e67cf0c41688ab135113
Nic Costa <nic.costa@gmail.com>
2019-03-07 15:08:25 -0600

    Add note about why the workaround was needed

--- recipes-rpi/hci-init-rpi/hci-init-rpi/hci-init-rpi.sh
+++ recipes-rpi/hci-init-rpi/hci-init-rpi/hci-init-rpi.sh
@@ -12,6 +12,15 @@
 if [ "$1" = "start" ]; then
     /usr/bin/btuart
     /usr/bin/bthelper hci0
+
+    # Jiggle the hci interface to help it start.
+    # Note: the next two lines aren't needed if the previous 2
+    # commands are executed manually from the command line after
+    # startup, so there seems to be a timing issue with the
+    # bluetooth interface and this init script.
+    # TODO: figure out the timing issue to understand why bthelper
+    # alone isn't enough to bring up the interface the first time
+    # it's called.
     /usr/bin/hciconfig hci0 down
     /usr/bin/hciconfig hci0 up
 else
