da837b193e5dd0e5f144f7257b2ab0629ce864cd
ashishwigwag <ashumittal0806@gmail.com>
2019-10-31 18:27:35 +0530

    PELEDGE19-936 md5 to validate vers mismatch for update campaign

--- recipes-wigwag/mbed-edge-core/files/rpi3/arm_update_activate.sh
+++ recipes-wigwag/mbed-edge-core/files/rpi3/arm_update_activate.sh
@@ -19,6 +19,9 @@ VALUE=$(cp $HEADER /userdata/extended/header.bin)
 mkdir -p /userdata/.logs-before-upgrade
 cp -R /wigwag/log/* /userdata/.logs-before-upgrade/
 
+# save version checksum
+md5sum /wigwag/etc/versions.json > /userdata/mbed/version_checksum.md5
+
 systemctl stop maestro
 systemctl start maestro
 
